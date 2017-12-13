ab64824103b677ad787cc70bbda3ac4fa205c92a
Seres Gyozo <osereso@gmail.com>
2017-12-13 12:08:05 +0100

    Added new answer id to data_manager.py and added add_new_asnwer to data_manager.py and finished route display in server.py.

--- data_manager.py
+++ data_manager.py
@@ -22,3 +22,19 @@ def get_answers_by_question_id(id):
         if answer_dict['question_id'] == id:
             answers_for_question.append(answer_dict)
     return answers_for_question
+
+
+def get_new_answer_id():
+    max_id = 0
+    data = connection.get_data_from_file(ANSWERS_FILE_NAME)
+    for num in range(1, len(data)):
+        act_id = int(data[num][connection.DATA_HEADER_ANSWER[0]])
+        if act_id > max_id:
+            max_id = act_id
+    return max_id + 1
+
+
+def add_new_answer(answer):
+    all_answers = connection.get_data_from_file(ANSWERS_FILE_NAME)
+    all_answers.append(answer)
+    connection.write_data_to_file(all_answers, connection.DATA_HEADER_ANSWER, ANSWERS_FILE_NAME)
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 
 import data_manager
+import time
 
 app = Flask(__name__)
 URL_INDEX = '/'
@@ -9,6 +10,8 @@ URL_DISPLAY = '/display/'
 
 @app.route(URL_INDEX)
 def route_index():
+    print(time.time())
+    print(time.localtime(time.time()))
     return render_template(
         'index.html',
         questions=data_manager.list_all_questions())
@@ -21,14 +24,15 @@ def route_display(question_id):
             'display_question.html',
             question=data_manager.get_question_by_id(question_id),
             answers=data_manager.get_answers_by_question_id(question_id))
-    else:
-        if request.method == 'POST':
-            answer = {'id': '',
-                      "submission_time": '',
-                      'vote_number': '',
-                      'question_id': question_id,
-                      'message': '',
-                      'image': ''}
+    else: # method POST
+        answer = {'id': str(data_manager.get_new_answer_id()),
+                  "submission_time": '',
+                  'vote_number': '',
+                  'question_id': question_id,
+                  'message': request.form['answer'],
+                  'image': ''}
+        data_manager.add_new_answer(answer)
+        return redirect(URL_DISPLAY + question_id)
 
 
 if __name__ == '__main__':
@@ -37,3 +41,4 @@ if __name__ == '__main__':
         port=5000,
         debug=True,
     )
+
