1d9f9192eb37d721c6a4be9422fae79f24ba7313
Adrienn Dánics <adrienn.d91@gmail.com>
2017-12-14 22:04:15 +0100

    Fixed/added edit features to server.py and html templates ask, display and post

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import data_manager
 import connection
@@ -27,7 +27,7 @@ def route_index():
 @app.route(URL_ASK, methods=['GET', 'POST'])
 def route_ask():
     if request.method == 'GET':
-        return render_template('ask.html')
+        return render_template('ask.html', req_url=url_for('route_ask'), question={})
     else:
 
         file = request.files['file']
@@ -37,12 +37,10 @@ def route_ask():
 
         question = {"id": str(data_manager.get_new_a_q_id(data_manager.QUESTION_FILE_NAME,
                                                           connection.DATA_HEADER_QUESTION)),
-                    "submission_time": str(utility.display_unix_time()),
-                    "view_number": '0',
-                    "vote_number": '0',
-                    'title': request.form['title'],
+                    "submission_time": str(utility.display_unix_time()), "view_number": '0',
+                    "vote_number": '0', 'title': request.form['title'],
                     'message': request.form['message'],
-                    'image': UPLOAD_FOLDER + '/' + filename}
+                    'image': UPLOAD_FOLDER + '/' + filename if file.filename else ''}
         data_manager.add_new_a_q(question, data_manager.QUESTION_FILE_NAME, connection.DATA_HEADER_QUESTION)
         return redirect(URL_DISPLAY + question['id'])
 
@@ -50,7 +48,8 @@ def route_ask():
 @app.route(URL_POST_ANSWER + '<question_id>')
 def route_answer(question_id):
     return render_template('post_answer.html', question_id=question_id,
-                           question=data_manager.get_question_by_id(question_id))
+                           question=data_manager.get_question_by_id(question_id),
+                           req_url=url_for('route_answer', question_id=question_id), answers={})
 
 
 @app.route(URL_DISPLAY + '<question_id>', methods=['POST', 'GET'])
@@ -68,10 +67,8 @@ def route_display(question_id):
     else:  # method POST
         answer = {'id': str(data_manager.get_new_a_q_id(data_manager.ANSWERS_FILE_NAME,
                                                                     connection.DATA_HEADER_ANSWER)),
-                  "submission_time": str(utility.display_unix_time()),
-                  'vote_number': '0',
-                  'question_id': question_id,
-                  'message': request.form['answer'],
+                  "submission_time": str(utility.display_unix_time()), 'vote_number': '0',
+                  'question_id': question_id, 'message': request.form['answer'],
                   'image': ''}
         data_manager.add_new_a_q(answer, data_manager.ANSWERS_FILE_NAME, connection.DATA_HEADER_ANSWER)
         return redirect(URL_DISPLAY + question_id)
@@ -86,12 +83,9 @@ def route_counter_question(question_id, direction):
     else:
         votes = int(question['vote_number'])
         votes -= 1
-    updated_votes = {'id': question['id'],
-                     "submission_time": question["submission_time"],
-                     'view_number': question['view_number'],
-                     'vote_number': votes,
-                     'title': question['title'],
-                     'message': question['message'],
+    updated_votes = {'id': question['id'], "submission_time": question["submission_time"],
+                     'view_number': question['view_number'], 'vote_number': votes,
+                     'title': question['title'], 'message': question['message'],
                      'image': question['image']}
     data_manager.update_q_and_a(updated_votes, data_manager.QUESTION_FILE_NAME, connection.DATA_HEADER_QUESTION)
     return redirect(URL_DISPLAY + question_id)
@@ -109,15 +103,52 @@ def route_counter_answer(question_id, response_id, direction):
     else:
         votes = int(answers['vote_number'])
         votes -= 1
-    updated_votes = {'id': answers['id'],
-                     "submission_time": answers["submission_time"],
-                     'vote_number': votes,
-                     'question_id': answers['question_id'],
-                     'message': answers['message'],
-                     'image': answers['image']}
+    updated_votes = {'id': answers['id'], "submission_time": answers["submission_time"],
+                     'vote_number': votes, 'question_id': answers['question_id'],
+                     'message': answers['message'], 'image': answers['image']}
     data_manager.update_q_and_a(updated_votes, data_manager.ANSWERS_FILE_NAME, connection.DATA_HEADER_ANSWER)
     return redirect(URL_DISPLAY + question_id)
 
+
+@app.route('/display/<question_id>/edit', methods=['GET'])
+def route_edit_question(question_id):
+    question = data_manager.get_question_by_id(question_id)
+    return render_template('ask.html', question=question,
+                           req_url=url_for('route_edit_question_save', question_id=question_id), question_id=question_id)
+
+@app.route('/display/<question_id>/edit/save', methods=['POST'])
+def route_edit_question_save(question_id):
+    question = data_manager.get_question_by_id(question_id)
+    updated_votes = {'id': question['id'], "submission_time": question["submission_time"],
+                     'view_number': question['view_number'], 'vote_number': question['vote_number'],
+                     'title': request.form['title'], 'message': request.form['message'],
+                     'image': question['image']}
+    data_manager.update_q_and_a(updated_votes, data_manager.QUESTION_FILE_NAME, connection.DATA_HEADER_QUESTION)
+    return redirect(URL_DISPLAY + question_id)
+
+
+@app.route('/display/<question_id>/<response_id>/edit', methods=['GET'])
+def route_edit_answer(question_id, response_id):
+    ans = data_manager.get_answers_by_question_id(question_id)
+    for item in ans:
+        if item['id'] == response_id:
+            answers = item
+    if request.method == 'GET':
+        return render_template('post_answer.html', answers=answers, question=data_manager.get_question_by_id(question_id),
+                               req_url=url_for('route_edit_answer_save', question_id=question_id, response_id=response_id),
+                               response_id=answers['id'], question_id=question_id)
+
+@app.route('/display/<question_id>/<response_id>/edit/save', methods=['POST'])
+def route_edit_answer_save(question_id, response_id):
+    ans = data_manager.get_answers_by_question_id(question_id)
+    for item in ans:
+        if item['id'] == response_id:
+            answers = item
+    updated_votes = {'id': answers['id'], "submission_time": answers["submission_time"],
+                     'vote_number': answers['vote_number'], 'question_id': answers['question_id'],
+                     'message': request.form['answer'], 'image': answers['image']}
+    data_manager.update_q_and_a(updated_votes, data_manager.ANSWERS_FILE_NAME, connection.DATA_HEADER_ANSWER)
+    return redirect(URL_DISPLAY + question_id)
 
 if __name__ == '__main__':
     app.run(
