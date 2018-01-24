2cf32aafde7d842fd6954a152337ea2f29501b98
Adrienn <adrienn.d91@gmail.com>
2018-01-24 11:52:27 +0100

    Delete comment button, query, server-side

--- data_manager.py
+++ data_manager.py
@@ -129,3 +129,8 @@ def search_answer(cursor, pattern):
                     """,
                    {'pattern': '%' + pattern + '%'})
     return cursor.fetchall()
+
+
+@connection.connection_handler
+def delete_comment(cursor, comment_id):
+    cursor.execute("""DELETE FROM comment WHERE id = %(cid)s;""", {'cid': comment_id})
